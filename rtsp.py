63e14b96c3831b1c3438721f3d74a2be986d8170
TosiGui <32494397+TosiGui@users.noreply.github.com>
2022-11-30 21:24:41 -0300

    feat(py): att to present to teachers

--- rtsp.py
+++ rtsp.py
@@ -1,16 +1,28 @@
 import cv2
 import requests
+from pymongo import MongoClient
+from fastapi import FastAPI
+from dotenv import dotenv_values
 
-cap = cv2.VideoCapture("rtsp://admin:pepsizero0@192.168.0.151:554/cam/realmonitor?channel=1&subtype=0")
-detector = cv2.QRCodeDetector()
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    data, bbox, _ = detector.detectAndDecode(frame)
-    cv2.imshow('frame', frame)
-    print(data)
-    if(data == "https://pt.wikipedia.org"):
-        requests.get(url='https://maker.ifttt.com/trigger/chamar_alexa/json/with/key/dn_6ohL8PU0W4byjYLYsYr')
-    if cv2.waitKey(20) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["tcc"]
+user = mydb["customers"]
+
+for x in mycol.find({},{ "name": 1, "urlRTSP": 1, "urlIFTTT": 1, "password": 1 }):
+    cap = cv2.VideoCapture(x.urlRTSP)
+    detector = cv2.QRCodeDetector()
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        data, bbox, _ = detector.detectAndDecode(frame)
+        cv2.imshow('frame', frame)
+        print(data)
+        if(data == password):
+            requests.get(url=x.urlIFTTT)
+        if cv2.waitKey(20) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    app.mongodb_client.close()
+
+def shutdown_db_client():
+    app.mongodb_client.close()
